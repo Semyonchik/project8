0cab6d6373abc96f2ca05f6ccef2c104efc4c4cf
teCemka <47112461+Semyonchik@users.noreply.github.com>
2022-03-21 22:46:17 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-a=curl -I http://192.168.88.74:9889 2>/dev/null | head -n 1 | cut -d$' ' -f2
-b=md5sum /tmp/test/index.html
+a=$(curl -I http://192.168.88.74:9889 2>/dev/null | head -n 1 | cut -d$' ' -f2)
+b=sudo md5sum /tmp/test/index.html | cut -d ' ' -f 1
 wget http://192.168.88.74:9889 -P /tmp/test/1/
-c=md5sum /tmp/test/1/index.html
-if [  $a != 200 ] && [ $b != $c ]
+c=sudo md5sum /tmp/test/1/index.html | cut -d ' ' -f 1
+if [[ $a != 200 || "$b" != "$c" ]]
 then
-  echo 1111
-else 
-  echo 2222
+  exit 2
+else
+  exit 1
 fi
