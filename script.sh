99ce88a45639fad8671d4922b7b7d236dd95b790
teCemka <47112461+Semyonchik@users.noreply.github.com>
2022-03-22 20:48:16 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ wget http://192.168.88.74:9889 -P /tmp/test/1/
 c=sudo md5sum /tmp/test/1/index.html | cut -d ' ' -f 1
 if [[ $a != 200 || "$b" != "$c" ]]
 then
-  echo 2
+  exit 4
 else
-  echo 1
+  exit 5
 fi
